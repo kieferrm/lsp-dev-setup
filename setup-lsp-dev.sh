3a322db8bc0df29694a78784f996c97495929e04
kieferrm <kieferrm@gmail.com>
2018-03-21 14:16:25 -0700

    modify setup scripts

--- setup-lsp-dev.sh
+++ setup-lsp-dev.sh
@@ -3,25 +3,27 @@
 mkdir lsp-dev
 cd lsp-dev
 
+# git clone https://github.com/Microsoft/vscode-languageserver-node.git
 # git clone git@github.com:Microsoft/vscode-languageserver-node.git
-git clone https://github.com/kieferrm/vscode-languageserver-node.git
+# git clone https://github.com/kieferrm/vscode-languageserver-node.git
+git clone git@github.com:kieferrm/vscode-languageserver-node.git
 cd vscode-languageserver-node
-git checkout test271
 npm install
 cd ..
 
+# git clone https://github.com/Microsoft/vscode-extension-samples.git
 # git clone git@github.com:Microsoft/vscode-extension-samples.git
-git clone https://github.com/kieferrm/vscode-extension-samples.git
+# git clone https://github.com/kieferrm/vscode-extension-samples.git
+git clone git@github.com:kieferrm/vscode-extension-samples.git
 cd vscode-extension-samples/lsp-sample
-git checkout test271
 npm install
 cd ../..
 
 
 # patch vscode.d.ts to use regular modules rather than global definitions
 SCRIPTDIR=`dirname $0`
-cp -f ../${SCRIPTDIR}/vscode-definitions/1.18/vscode.d.ts vscode-languageserver-node/client/node_modules/vscode 
-cp -f ../${SCRIPTDIR}/vscode-definitions/1.16/vscode.d.ts vscode-extension-samples/lsp-sample/client/node_modules/vscode
+cp -f ../${SCRIPTDIR}/vscode-definitions/1.22.prerelease/vscode.d.ts vscode-languageserver-node/client/node_modules/vscode 
+cp -f ../${SCRIPTDIR}/vscode-definitions/1.22.prerelease/vscode.d.ts vscode-extension-samples/lsp-sample/client/node_modules/vscode
 
 
 FOLDER=`pwd`
